25ee6fa317bc78e907f0cca2d5b3e4ec07e237af
Ashinator <Ahart425@gmail.com>
2019-03-16 09:53:37 -0600

    setting up landing page

---
+++ javascript/story.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $("nextArrowTwo").hide();
+    $("secondLine").hide();
+
+    $("#nextArrowOne").on("click", function() {
+        $("nextArrowTwo").show();
+        $("secondLine").show();
+        
+
+
+    });
+
+// On click function to prompt users through beginning - Click next to read rules - Enter word and click next to enter another word - pretty box
+
+// Make an array/loop to allow users to enter their own words
+
+// Append user entered words into the story line using a loop - Make all user words CAPITAL
+
+
+
+
+
+
+
+}); // END DOCUMENT ON-READY
